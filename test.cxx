441af9e66d3ace89d3e4e3704ce1342e652aea73
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2005-11-09 21:21:12 +0100

    add template cxx file
    darcs-hash:20051109202112-2fc9d-d0c92f7d5b04fbad7a2b1d705e77ccc2bbe74847.gz

---
+++ test.cxx
@@ -0,0 +1,73 @@
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkCommand.h"
+
+#include "itkImageFilter.h"
+
+template < class TFilter >
+class ProgressCallback : public itk::Command
+{
+public:
+  typedef ProgressCallback   Self;
+  typedef itk::Command  Superclass;
+  typedef itk::SmartPointer<Self>  Pointer;
+  typedef itk::SmartPointer<const Self>  ConstPointer;
+
+  itkTypeMacro( IterationCallback, Superclass );
+  itkNewMacro( Self );
+  
+  /** Type defining the optimizer. */
+  typedef    TFilter     FilterType;
+
+  /** Method to specify the optimizer. */
+  void SetFilter( FilterType * filter )
+    {
+    m_Filter = filter;
+    m_Filter->AddObserver( itk::ProgressEvent(), this );
+    }
+
+  /** Execute method will print data at each iteration */
+  void Execute(itk::Object *caller, const itk::EventObject & event)
+    {
+    Execute( (const itk::Object *)caller, event);
+    }
+    
+  void Execute(const itk::Object *, const itk::EventObject & event)
+    {
+    std::cout << m_Filter->GetNameOfClass() << ": " << m_Filter->GetProgress() << std::endl;
+    }
+
+protected:
+  ProgressCallback() {};
+  itk::WeakPointer<FilterType>   m_Filter;
+};
+
+
+int main(int, char * argv[])
+{
+  const int dim = 2;
+  
+  typedef unsigned char PType;
+  typedef itk::Image< PType, dim > IType;
+
+  typedef itk::ImageFileReader< IType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+  typedef itk::ImageFilter< IType, IType > FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( reader->GetOutput() );
+
+  typedef ProgressCallback< FilterType > ProgressType;
+  ProgressType::Pointer progress = ProgressType::New();
+  progress->SetFilter( filter );
+
+  typedef itk::ImageFileWriter< IType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( filter->GetOutput() );
+  writer->SetFileName( argv[2] );
+  writer->Update();
+
+  return 0;
+}
+
