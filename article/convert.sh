0c22fda9e0379443a00cf6118cd5918c538e9cc5
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2005-12-16 23:15:22 +0100

    remove convert - the job is done by the make file
    darcs-hash:20051216221522-2fc9d-4a99f5e0e81b3f2715fdecf8bc10ed4559a8a4e5.gz

--- article/convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-for ext in png PNG jpg JPG jpeg JPEG ; do
-  for f in `find -name "*.$ext"` ; do
-    dest=`basename $f .$ext`.eps
-    echo "$f -> $dest"
-    convert $f $dest
-  done
-done
