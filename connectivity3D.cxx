80215b873496dba9d38e8b3c3a1027c407181dfb
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-09-05 10:44:43 +0200

    update tests
    darcs-hash:20070905084443-2fc9d-c5b63d7f4d2a48e4c61d309eaf44eabd992f5818.gz

--- connectivity3D.cxx
+++ connectivity3D.cxx
@@ -7,87 +7,32 @@ int main(int argc, char** argv)
 
   const unsigned int Dimension = 3;
   typedef itk::Connectivity< Dimension > ConnectivityType;
+
   ConnectivityType::Pointer connectivity = ConnectivityType::New();
-  
-  std::cout << "global cell dimension: " << ConnectivityType::GetGlobalDefaultCellDimension() << std::endl;
-  std::cout << "global fully connected: " << ConnectivityType::GetGlobalDefaultFullyConnected() << std::endl;
-
-  std::cout << std::endl;
-
-  std::cout << "cell dimension: " << connectivity->GetCellDimension() << std::endl;
-  std::cout << "fully connected: " << connectivity->GetFullyConnected() << std::endl;
-  std::cout << "number of neighbors: " << connectivity->GetNumberOfNeighbors() << std::endl;
-  std::cout << "computed number of neighbors: " << connectivity->ComputeNumberOfNeighbors() << std::endl;
-  std::cout << "neighbors: ";
-  for( int i=0; i<connectivity->GetNumberOfNeighbors(); i++ )
-    {
-    std::cout << connectivity->GetNeighbors()[i] << " ";
-    }
-  std::cout << std::endl;
-
+  connectivity->Print( std::cout );
   std::cout << std::endl;
 
   connectivity->SetFullyConnected( true );
-  std::cout << "cell dimension: " << connectivity->GetCellDimension() << std::endl;
-  std::cout << "fully connected: " << connectivity->GetFullyConnected() << std::endl;
-  std::cout << "number of neighbors: " << connectivity->GetNumberOfNeighbors() << std::endl;
-  std::cout << "computed number of neighbors: " << connectivity->ComputeNumberOfNeighbors() << std::endl;
-  std::cout << "neighbors: ";
-  for( int i=0; i<connectivity->GetNumberOfNeighbors(); i++ )
-    {
-    std::cout << connectivity->GetNeighbors()[i] << " ";
-    }
-  std::cout << std::endl;
-  
+  connectivity->Print( std::cout );
   std::cout << std::endl;
 
   connectivity->SetFullyConnected( false );
-  std::cout << "cell dimension: " << connectivity->GetCellDimension() << std::endl;
-  std::cout << "fully connected: " << connectivity->GetFullyConnected() << std::endl;
-  std::cout << "number of neighbors: " << connectivity->GetNumberOfNeighbors() << std::endl;
-  std::cout << "computed number of neighbors: " << connectivity->ComputeNumberOfNeighbors() << std::endl;
-  std::cout << "neighbors: ";
-  for( int i=0; i<connectivity->GetNumberOfNeighbors(); i++ )
-    {
-    std::cout << connectivity->GetNeighbors()[i] << " ";
-    }
-  std::cout << std::endl;
-
-  std::cout << std::endl;
-
-  connectivity->SetCellDimension( 1 );
-  std::cout << "cell dimension: " << connectivity->GetCellDimension() << std::endl;
-  std::cout << "fully connected: " << connectivity->GetFullyConnected() << std::endl;
-  std::cout << "number of neighbors: " << connectivity->GetNumberOfNeighbors() << std::endl;
-  std::cout << "computed number of neighbors: " << connectivity->ComputeNumberOfNeighbors() << std::endl;
-  std::cout << "neighbors: ";
-  for( int i=0; i<connectivity->GetNumberOfNeighbors(); i++ )
-    {
-    std::cout << connectivity->GetNeighbors()[i] << " ";
-    }
-  std::cout << std::endl;
-  
+  connectivity->Print( std::cout );
   std::cout << std::endl;
 
   ConnectivityType::SetGlobalDefaultFullyConnected( true );
-
-  std::cout << "global cell dimension: " << ConnectivityType::GetGlobalDefaultCellDimension() << std::endl;
-  std::cout << "global fully connected: " << ConnectivityType::GetGlobalDefaultFullyConnected() << std::endl;
-
-  std::cout << std::endl;
-
   connectivity = ConnectivityType::New();
+  connectivity->Print( std::cout );
+  std::cout << std::endl;
   
-  std::cout << "cell dimension: " << connectivity->GetCellDimension() << std::endl;
-  std::cout << "fully connected: " << connectivity->GetFullyConnected() << std::endl;
-  std::cout << "number of neighbors: " << connectivity->GetNumberOfNeighbors() << std::endl;
-  std::cout << "computed number of neighbors: " << connectivity->ComputeNumberOfNeighbors() << std::endl;
-  std::cout << "neighbors: ";
-  for( int i=0; i<connectivity->GetNumberOfNeighbors(); i++ )
-    {
-    std::cout << connectivity->GetNeighbors()[i] << " ";
-    }
-  std::cout << std::endl;    
+  connectivity->SetCellDimension( 3 );
+  connectivity->Print( std::cout );
+  std::cout << std::endl;
+  
+  ConnectivityType::SetGlobalDefaultNumberOfNeighbors( 18 );
+  connectivity = ConnectivityType::New();
+  connectivity->Print( std::cout );
+  std::cout << std::endl;
   
   return EXIT_SUCCESS;
 }
